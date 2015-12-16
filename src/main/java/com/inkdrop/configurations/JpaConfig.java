640a5619429e9e113a140fae927725543db5a89d
Luiz Eduardo <luizeduardokowalski@gmail.com>
2015-12-16 16:20:50 -0200

    Added hikariCP

---
+++ src/main/java/com/inkdrop/configurations/JpaConfig.java
@@ -0,0 +1,23 @@
+package com.inkdrop.configurations;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ConfigurationProperties(prefix = "params.datasource")
+public class JpaConfig extends HikariConfig {
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        return new HikariDataSource(this);
+    }
+
+}
