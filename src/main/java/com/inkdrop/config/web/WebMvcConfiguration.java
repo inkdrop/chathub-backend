d4897e9506efdb18bc0f6bde34766e291aaff6ed
Luiz Eduardo <luizeduardokowalski@gmail.com>
2016-05-18 11:01:14 +0200

    Addeed CORS

--- src/main/java/com/inkdrop/config/web/WebMvcConfiguration.java
+++ src/main/java/com/inkdrop/config/web/WebMvcConfiguration.java
@@ -1,9 +1,15 @@
 package com.inkdrop.config.web;
 
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfiguration extends WebMvcAutoConfiguration{}
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter{
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/").allowedOrigins("*");
+	}
+}
