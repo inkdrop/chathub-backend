dc667665739596f6ba05ff1b863fb801b03b76b8
Luiz Eduardo <luizeduardokowalski@gmail.com>
2016-08-11 22:37:26 +0200

    Caching with caffeine

--- src/main/java/com/inkdrop/config/cache/CacheConfiguration.java
+++ src/main/java/com/inkdrop/config/cache/CacheConfiguration.java
@@ -1,44 +1,8 @@
 package com.inkdrop.config.cache;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @EnableCaching
-@Profile("docker")
-public class CacheConfiguration {
-
-	@Value("${cache.host}")
-	private String cacheHost;
-
-	@Value("${cache.port}")
-	private int cachePort;
-
-	@Bean
-	JedisConnectionFactory jedisConnectionFactory() {
-		JedisConnectionFactory factory = new JedisConnectionFactory();
-		factory.setHostName(cacheHost);
-		factory.setPort(cachePort);
-		factory.setUsePool(true);
-		return factory;
-	}
-
-	@Bean
-	RedisTemplate<Object, Object> redisTemplate() {
-		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(jedisConnectionFactory());
-		return redisTemplate;
-	}
-
-	@Bean
-	CacheManager cacheManager() {
-		return new RedisCacheManager(redisTemplate());
-	}
-}
+public class CacheConfiguration {}
