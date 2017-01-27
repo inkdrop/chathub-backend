657de0b917d9e2b7497564d8924d3ba57a1265d8
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2017-01-27 12:05:39 +0100

    adding distributed tracing

--- src/main/java/com/inkdrop/ChathubApp.java
+++ src/main/java/com/inkdrop/ChathubApp.java
@@ -3,12 +3,19 @@ package com.inkdrop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @ServletComponentScan
 @EnableAspectJAutoProxy
 public class ChathubApp {
+
+	@Bean
+	public AlwaysSampler defaultSampler() {
+		return new AlwaysSampler();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChathubApp.class, args);
